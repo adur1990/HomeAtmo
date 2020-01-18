8d901cd98bb9d1341dc53c6781abf646d89499cc
Artur <git@artursterz.de>
2020-01-18 22:26:37 +0100

    Make tabspace equal to 4 everywhere

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@
 
 /******* TIMER VARIABLES *******/
 bool sent_today = false;
-hw_timer_t * timer = NULL;
+hw_timer_t *timer = NULL;
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
 uint64_t day = 1000000 * 60 * 60 * 24;
 uint64_t hour = 1000 * 60 * 60;
@@ -26,10 +26,10 @@ HTTPClient http;
 
 /******* SENSOR VARIABLES *******/
 const uint8_t bsec_config_iaq[] = {
-  #include "bsec_iaq.txt"
+    #include "bsec_iaq.txt"
 };
 
-#define STATE_SAVE_PERIOD	UINT32_C(360 * 60 * 1000) // 360 minutes - 4 times a day
+#define STATE_SAVE_PERIOD UINT32_C(360 * 60 * 1000) // 360 minutes - 4 times a day
 
 Bsec sensor;
 uint8_t bsec_state[BSEC_MAX_STATE_BLOB_SIZE] = {0};
@@ -46,9 +46,9 @@ float iaq = 0.0;
 float co2Equivalent = 0.0;
 
 void IRAM_ATTR reset_sent() {
-  portENTER_CRITICAL_ISR(&timerMux);
-  sent_today = false;
-  portEXIT_CRITICAL_ISR(&timerMux);
+    portENTER_CRITICAL_ISR(&timerMux);
+    sent_today = false;
+    portEXIT_CRITICAL_ISR(&timerMux);
 }
 
 void setup() {
@@ -73,11 +73,11 @@ void setup() {
     load_state();
 
     bsec_virtual_sensor_t sensor_list[5] = {
-      BSEC_OUTPUT_IAQ,
-      BSEC_OUTPUT_CO2_EQUIVALENT,
-      BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_TEMPERATURE,
-      BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_HUMIDITY,
-      BSEC_OUTPUT_RAW_PRESSURE
+        BSEC_OUTPUT_IAQ,
+        BSEC_OUTPUT_CO2_EQUIVALENT,
+        BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_TEMPERATURE,
+        BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_HUMIDITY,
+        BSEC_OUTPUT_RAW_PRESSURE
     };
 
     sensor.updateSubscription(sensor_list, 5, BSEC_SAMPLE_RATE_LP);
@@ -91,28 +91,29 @@ void setup() {
         Serial.print(".");
     }
     Serial.println("");
+
     Serial.println("#### WiFi connected.");
     Serial.println("#### IP address: ");
     Serial.println(WiFi.localIP());
 
     // Route for root / web page
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-      request->send(200, "text/html", INDEX_HTML);
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+        request->send(200, "text/html", INDEX_HTML);
     });
-    server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
-      request->send_P(200, "text/plain", String(temperature).c_str());
+    server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request) {
+        request->send_P(200, "text/plain", String(temperature).c_str());
     });
-    server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
-      request->send_P(200, "text/plain", String(humidity).c_str());
+    server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request) {
+        request->send_P(200, "text/plain", String(humidity).c_str());
     });
-    server.on("/pressure", HTTP_GET, [](AsyncWebServerRequest *request){
-      request->send_P(200, "text/plain", String(pressure / 100.0F).c_str());
+    server.on("/pressure", HTTP_GET, [](AsyncWebServerRequest *request) {
+        request->send_P(200, "text/plain", String(pressure / 100.0F).c_str());
     });
-    server.on("/iaq", HTTP_GET, [](AsyncWebServerRequest *request){
-      request->send_P(200, "text/plain", String(iaq).c_str());
+    server.on("/iaq", HTTP_GET, [](AsyncWebServerRequest *request) {
+        request->send_P(200, "text/plain", String(iaq).c_str());
     });
-    server.on("/co2", HTTP_GET, [](AsyncWebServerRequest *request){
-      request->send_P(200, "text/plain", String(co2Equivalent).c_str());
+    server.on("/co2", HTTP_GET, [](AsyncWebServerRequest *request) {
+        request->send_P(200, "text/plain", String(co2Equivalent).c_str());
     });
 
     server.begin();
@@ -120,112 +121,114 @@ void setup() {
 }
 
 void loop() {
-    if(millis() - last_time > hour && !sent_today) {
-      if (iaq > 50) {
-        portENTER_CRITICAL(&timerMux);
-        sent_today = true;
-        portEXIT_CRITICAL(&timerMux);
-        Serial.println("BAD QUALITY");
+    if (millis() - last_time > hour && !sent_today) {
+        if (iaq > 50) {
+            portENTER_CRITICAL(&timerMux);
+            sent_today = true;
+            portEXIT_CRITICAL(&timerMux);
+            Serial.println("BAD QUALITY");
 
-        http.begin(IFTTT);
-        http.addHeader("Content-Type", "application/json");
-        int httpResponseCode = http.POST("{\"value1\":\"" + String(iaq) + "\"}");
+            http.begin(IFTTT);
+            http.addHeader("Content-Type", "application/json");
+            int httpResponseCode = http.POST("{\"value1\":\"" + String(iaq) + "\"}");
 
-        if (httpResponseCode > 0) {
-          String response = http.getString();
-          Serial.println(httpResponseCode);
-          Serial.println(response);
-        } else {
-          Serial.print("Error on sending POST: ");
-          Serial.println(httpResponseCode);
+            if (httpResponseCode > 0) {
+                String response = http.getString();
+                Serial.println(httpResponseCode);
+                Serial.println(response);
+            } else {
+                Serial.print("Error on sending POST: ");
+                Serial.println(httpResponseCode);
+            }
+
+            http.end();
         }
-
-        http.end();
-      }
-      last_time = millis();
+        last_time = millis();
     }
 
-  if (sensor.run()) {
-    temperature = sensor.temperature;
-    pressure = sensor.pressure;
-    humidity = sensor.humidity;
-    iaq = sensor.iaq;
-    co2Equivalent = sensor.co2Equivalent;
-    update_state();
-  } else {
-    check_IAQ_sensor_status();
-  }
+    if (sensor.run()) {
+        temperature = sensor.temperature;
+        pressure = sensor.pressure;
+        humidity = sensor.humidity;
+        iaq = sensor.iaq;
+        co2Equivalent = sensor.co2Equivalent;
+        update_state();
+    } else {
+        check_IAQ_sensor_status();
+    }
 }
 
 void check_IAQ_sensor_status(void) {
-  if (sensor.status != BSEC_OK) {
-    if (sensor.status < BSEC_OK) {
-      Serial.println("#### BSEC error: " + String(sensor.status));
-      for (;;);
-    } else {
-      Serial.println("#### BSEC warning: " + String(sensor.status));
+    if (sensor.status != BSEC_OK) {
+        if (sensor.status < BSEC_OK) {
+            Serial.println("#### BSEC error: " + String(sensor.status));
+            for (;;)
+                ;
+        } else {
+            Serial.println("#### BSEC warning: " + String(sensor.status));
+        }
     }
-  }
 
-  if (sensor.bme680Status != BME680_OK) {
-    if (sensor.bme680Status < BME680_OK) {
-      Serial.println("#### BME680 error: " + String(sensor.bme680Status));
-      for (;;);
-    } else {
-      Serial.println("#### BME680 warning: " + String(sensor.bme680Status));
+    if (sensor.bme680Status != BME680_OK) {
+        if (sensor.bme680Status < BME680_OK) {
+            Serial.println("#### BME680 error: " + String(sensor.bme680Status));
+            for (;;)
+                ;
+        } else {
+            Serial.println("#### BME680 warning: " + String(sensor.bme680Status));
+        }
     }
-  }
 }
 
 void load_state(void) {
-  if (EEPROM.read(0) == BSEC_MAX_STATE_BLOB_SIZE) {
-    Serial.println("#### Reading state from EEPROM:");
+    if (EEPROM.read(0) == BSEC_MAX_STATE_BLOB_SIZE) {
+        Serial.println("#### Reading state from EEPROM:");
 
-    for (uint8_t i = 0; i < BSEC_MAX_STATE_BLOB_SIZE; i++) {
-      bsec_state[i] = EEPROM.read(i + 1);
-      Serial.print(bsec_state[i], HEX);
+        for (uint8_t i = 0; i < BSEC_MAX_STATE_BLOB_SIZE; i++) {
+            bsec_state[i] = EEPROM.read(i + 1);
+            Serial.print(bsec_state[i], HEX);
+        }
+        Serial.println("");
+
+        sensor.setState(bsec_state);
+        check_IAQ_sensor_status();
+    } else {
+        Serial.println("#### Erasing EEPROM");
+
+        for (uint8_t i = 0; i < BSEC_MAX_STATE_BLOB_SIZE + 1; i++) {
+            EEPROM.write(i, 0);
+        }
+
+        EEPROM.commit();
     }
-    Serial.println("");
-
-    sensor.setState(bsec_state);
-    check_IAQ_sensor_status();
-  } else {
-    Serial.println("#### Erasing EEPROM");
-
-    for (uint8_t i = 0; i < BSEC_MAX_STATE_BLOB_SIZE + 1; i++) {
-      EEPROM.write(i, 0);
-    }
-
-    EEPROM.commit();
-  }
 }
 
 void update_state(void) {
-  bool update = false;
-  if (state_update_counter == 0) {
-    if (sensor.iaqAccuracy >= 3) {
-      update = true;
-      state_update_counter++;
+    bool update = false;
+    if (state_update_counter == 0) {
+        if (sensor.iaqAccuracy >= 3) {
+            update = true;
+            state_update_counter++;
+        }
+    } else {
+        if ((state_update_counter * STATE_SAVE_PERIOD) < millis()) {
+            update = true;
+            state_update_counter++;
+        }
     }
-  } else {
-    if ((state_update_counter * STATE_SAVE_PERIOD) < millis()) {
-      update = true;
-      state_update_counter++;
+
+    if (update) {
+        sensor.getState(bsec_state);
+        check_IAQ_sensor_status();
+
+        Serial.println("#### Writing state to EEPROM:");
+        for (uint8_t i = 0; i < BSEC_MAX_STATE_BLOB_SIZE; i++) {
+            EEPROM.write(i + 1, bsec_state[i]);
+            Serial.println(bsec_state[i], HEX);
+        }
+        Serial.println("");
+
+        EEPROM.write(0, BSEC_MAX_STATE_BLOB_SIZE);
+        EEPROM.commit();
     }
-  }
-
-  if (update) {
-    sensor.getState(bsec_state);
-    check_IAQ_sensor_status();
-
-    Serial.println("#### Writing state to EEPROM:");
-    for (uint8_t i = 0; i < BSEC_MAX_STATE_BLOB_SIZE ; i++) {
-      EEPROM.write(i + 1, bsec_state[i]);
-      Serial.println(bsec_state[i], HEX);
-    }
-    Serial.println("");
-
-    EEPROM.write(0, BSEC_MAX_STATE_BLOB_SIZE);
-    EEPROM.commit();
-  }
 }
