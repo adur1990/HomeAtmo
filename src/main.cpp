5a4e74674e68f146234bd8aab862929743c7858f
Artur <git@artursterz.de>
2020-01-18 22:23:00 +0100

    Move to BSEC library for more accurate results

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
+/******* INCLUDES *******/
 #include <Arduino.h>
 
+#include <EEPROM.h>
 #include <Wire.h>
-#include <SPI.h>
-#include <Adafruit_Sensor.h>
-#include "Adafruit_BME680.h"
+#include "bsec.h"
 
 #include "WiFi.h"
 #include <HTTPClient.h>
@@ -12,7 +12,7 @@
 #include "html.h"
 #include "creds.h"
 
-/******* Timer Variables *******/
+/******* TIMER VARIABLES *******/
 bool sent_today = false;
 hw_timer_t * timer = NULL;
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
@@ -20,114 +20,35 @@ uint64_t day = 1000000 * 60 * 60 * 24;
 uint64_t hour = 1000 * 60 * 60;
 int last_time = 0;
 
-/******* Sensor Variables *******/
-#define SEALEVELPRESSURE_HPA (1013.25)
-
-Adafruit_BME680 bme; // I2C
-
-float hum_weighting = 0.25; // so hum effect is 25% of the total air quality score
-float gas_weighting = 0.75; // so gas effect is 75% of the total air quality score
-
-float hum_score, gas_score;
-float gas_reference = 250000;
-float hum_reference = 40;
-int   getgasreference_count = 0;
-
-/******* WiFi Variables *******/
+/******* WI-FI VARIABLES *******/
 AsyncWebServer server(80);
 HTTPClient http;
 
-String getTemperature() {
-  float t = bme.readTemperature();
-  if (isnan(t)) {
-    Serial.println("Failed to read from BME680 sensor!");
-    return "";
-  }
-  else {
-    Serial.println("Temperature: " + String(t));
-    return String(t);
-  }
-}
+/******* SENSOR VARIABLES *******/
+const uint8_t bsec_config_iaq[] = {
+  #include "bsec_iaq.txt"
+};
 
-String getHumidity() {
-  float h = bme.readHumidity();
-  if (isnan(h)) {
-    Serial.println("Failed to read from BME680 sensor!");
-    return "";
-  }
-  else {
-    Serial.println("Humidity: " + String(h));
-    return String(h);
-  }
-}
+#define STATE_SAVE_PERIOD	UINT32_C(360 * 60 * 1000) // 360 minutes - 4 times a day
 
-String getPressure() {
-  float p = bme.readPressure() / 100.0F;
-  if (isnan(p)) {
-    Serial.println("Failed to read from BME680 sensor!");
-    return "";
-  }
-  else {
-    Serial.println("Pressure: " + String(p));
-    return String(p);
-  }
-}
+Bsec sensor;
+uint8_t bsec_state[BSEC_MAX_STATE_BLOB_SIZE] = {0};
+uint16_t state_update_counter = 0;
 
-void getGasReference(){
-  // Now run the sensor for a burn-in period, then use combination of relative humidity and gas resistance to estimate indoor air quality as a percentage.
-  Serial.println("#### Getting a new gas reference value");
-  int readings = 5;
-  for (int i = 1; i <= readings; i++){
-    gas_reference += bme.readGas();
-  }
-  gas_reference = gas_reference / readings;
-}
+void check_IAQ_sensor_status(void);
+void load_state(void);
+void update_state(void);
 
-String getIAQ(){
-  //Calculate humidity contribution to IAQ index
-  float current_humidity = bme.readHumidity();
-  if (current_humidity >= 38 && current_humidity <= 42) {
-    hum_score = 0.25*100; // Humidity +/-5% around optimum
-  } else { //sub-optimal
-    if (current_humidity < 38) {
-      hum_score = 0.25 / hum_reference * current_humidity * 100;
-    } else {
-      hum_score = ((-0.25 / (100 - hum_reference) * current_humidity) + 0.416666) * 100;
-    }
-  }
+float temperature = 0.0;
+float pressure = 0.0;
+float humidity = 0.0;
+float iaq = 0.0;
+float co2Equivalent = 0.0;
 
-  //Calculate gas contribution to IAQ index
-  float gas_lower_limit = 5000;   // Bad air quality limit
-  float gas_upper_limit = 50000;  // Good air quality limit
-  float gas_boundary = gas_upper_limit - gas_lower_limit;
-
-  if (gas_reference > gas_upper_limit) {
-      gas_reference = gas_upper_limit;
-  }
-
-  if (gas_reference < gas_lower_limit) {
-      gas_reference = gas_lower_limit;
-  }
-
-  gas_score = (0.75 / gas_boundary * gas_reference - (gas_lower_limit * (0.75 / gas_boundary))) * 100;
-
-  //Combine results for the final IAQ index value (0-100% where 100% is good quality air)
-  float score = hum_score + gas_score;
-  float air_quality_score = (100 - score) * 5;
-
-  if ((++getgasreference_count) % 10 == 0) {
-      getGasReference();
-  }
-
-  Serial.println("IAQ: " + String(air_quality_score));
-  return String(air_quality_score);
-}
-
-void IRAM_ATTR resetSent() {
+void IRAM_ATTR reset_sent() {
   portENTER_CRITICAL_ISR(&timerMux);
   sent_today = false;
   portEXIT_CRITICAL_ISR(&timerMux);
-
 }
 
 void setup() {
@@ -135,28 +56,32 @@ void setup() {
 
     Serial.println("#### Setting up Timer");
     timer = timerBegin(0, 80, true);
-    timerAttachInterrupt(timer, &resetSent, true);
+    timerAttachInterrupt(timer, &reset_sent, true);
     timerAlarmWrite(timer, day, true);
     timerAlarmEnable(timer);
 
-    Serial.println("#### Setting up Sensors");
+    Serial.println("#### Setting up sensor.");
+    EEPROM.begin(BSEC_MAX_STATE_BLOB_SIZE + 1);
+
     Wire.begin();
-    if (!bme.begin()) {
-        Serial.println("#### Could not find a valid BME680 sensor, check wiring!");
-        while (1);
-    } else {
-        Serial.println("#### Found a sensor");
-    }
+    sensor.begin(BME680_I2C_ADDR_SECONDARY, Wire);
 
-    Serial.println("#### Calibrating Sensors");
-    bme.setTemperatureOversampling(BME680_OS_2X);
-    bme.setHumidityOversampling(BME680_OS_2X);
-    bme.setPressureOversampling(BME680_OS_2X);
-    bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
-    bme.setGasHeater(320, 150); // 320°C for 150 ms
-    // Now run the sensor for a burn-in period, then use combination of relative humidity and gas resistance to estimate indoor air quality as a percentage.
-    getGasReference();
+    check_IAQ_sensor_status();
+    sensor.setConfig(bsec_config_iaq);
+    check_IAQ_sensor_status();
 
+    load_state();
+
+    bsec_virtual_sensor_t sensor_list[5] = {
+      BSEC_OUTPUT_IAQ,
+      BSEC_OUTPUT_CO2_EQUIVALENT,
+      BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_TEMPERATURE,
+      BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_HUMIDITY,
+      BSEC_OUTPUT_RAW_PRESSURE
+    };
+
+    sensor.updateSubscription(sensor_list, 5, BSEC_SAMPLE_RATE_LP);
+    check_IAQ_sensor_status();
 
     Serial.print("#### Connecting to WiFi");
     WiFi.begin(SSID, PSK);
@@ -169,33 +94,33 @@ void setup() {
     Serial.println("#### WiFi connected.");
     Serial.println("#### IP address: ");
     Serial.println(WiFi.localIP());
-    Serial.println(WiFi.macAddress());
-
 
     // Route for root / web page
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
       request->send(200, "text/html", INDEX_HTML);
     });
     server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
-      request->send_P(200, "text/plain", getTemperature().c_str());
+      request->send_P(200, "text/plain", String(temperature).c_str());
     });
     server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
-      request->send_P(200, "text/plain", getHumidity().c_str());
+      request->send_P(200, "text/plain", String(humidity).c_str());
     });
     server.on("/pressure", HTTP_GET, [](AsyncWebServerRequest *request){
-      request->send_P(200, "text/plain", getPressure().c_str());
+      request->send_P(200, "text/plain", String(pressure / 100.0F).c_str());
     });
-        server.on("/iaq", HTTP_GET, [](AsyncWebServerRequest *request){
-      request->send_P(200, "text/plain", getIAQ().c_str());
+    server.on("/iaq", HTTP_GET, [](AsyncWebServerRequest *request){
+      request->send_P(200, "text/plain", String(iaq).c_str());
+    });
+    server.on("/co2", HTTP_GET, [](AsyncWebServerRequest *request){
+      request->send_P(200, "text/plain", String(co2Equivalent).c_str());
     });
 
-    Serial.println("#### Server Started.");
     server.begin();
+    Serial.println("#### Server Started.");
 }
 
 void loop() {
     if(millis() - last_time > hour && !sent_today) {
-      float iaq = getIAQ().toFloat();
       if (iaq > 50) {
         portENTER_CRITICAL(&timerMux);
         sent_today = true;
@@ -219,4 +144,88 @@ void loop() {
       }
       last_time = millis();
     }
+
+  if (sensor.run()) {
+    temperature = sensor.temperature;
+    pressure = sensor.pressure;
+    humidity = sensor.humidity;
+    iaq = sensor.iaq;
+    co2Equivalent = sensor.co2Equivalent;
+    update_state();
+  } else {
+    check_IAQ_sensor_status();
+  }
+}
+
+void check_IAQ_sensor_status(void) {
+  if (sensor.status != BSEC_OK) {
+    if (sensor.status < BSEC_OK) {
+      Serial.println("#### BSEC error: " + String(sensor.status));
+      for (;;);
+    } else {
+      Serial.println("#### BSEC warning: " + String(sensor.status));
+    }
+  }
+
+  if (sensor.bme680Status != BME680_OK) {
+    if (sensor.bme680Status < BME680_OK) {
+      Serial.println("#### BME680 error: " + String(sensor.bme680Status));
+      for (;;);
+    } else {
+      Serial.println("#### BME680 warning: " + String(sensor.bme680Status));
+    }
+  }
+}
+
+void load_state(void) {
+  if (EEPROM.read(0) == BSEC_MAX_STATE_BLOB_SIZE) {
+    Serial.println("#### Reading state from EEPROM:");
+
+    for (uint8_t i = 0; i < BSEC_MAX_STATE_BLOB_SIZE; i++) {
+      bsec_state[i] = EEPROM.read(i + 1);
+      Serial.print(bsec_state[i], HEX);
+    }
+    Serial.println("");
+
+    sensor.setState(bsec_state);
+    check_IAQ_sensor_status();
+  } else {
+    Serial.println("#### Erasing EEPROM");
+
+    for (uint8_t i = 0; i < BSEC_MAX_STATE_BLOB_SIZE + 1; i++) {
+      EEPROM.write(i, 0);
+    }
+
+    EEPROM.commit();
+  }
+}
+
+void update_state(void) {
+  bool update = false;
+  if (state_update_counter == 0) {
+    if (sensor.iaqAccuracy >= 3) {
+      update = true;
+      state_update_counter++;
+    }
+  } else {
+    if ((state_update_counter * STATE_SAVE_PERIOD) < millis()) {
+      update = true;
+      state_update_counter++;
+    }
+  }
+
+  if (update) {
+    sensor.getState(bsec_state);
+    check_IAQ_sensor_status();
+
+    Serial.println("#### Writing state to EEPROM:");
+    for (uint8_t i = 0; i < BSEC_MAX_STATE_BLOB_SIZE ; i++) {
+      EEPROM.write(i + 1, bsec_state[i]);
+      Serial.println(bsec_state[i], HEX);
+    }
+    Serial.println("");
+
+    EEPROM.write(0, BSEC_MAX_STATE_BLOB_SIZE);
+    EEPROM.commit();
+  }
 }
